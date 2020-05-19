46096c735cebbebb5af999d364f930329e102098
Luke MacKenzie <mackenzieluke94@gmail.com>
2020-05-18 21:53:59 -0400

    first

---
+++ gulpfile.js
@@ -0,0 +1,89 @@
+const del = require('del');
+const gulp = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+const runSequence = require('run-sequence');
+const terser = require('gulp-terser');
+const imagemin = require('gulp-imagemin');
+const purgecss = require('gulp-purgecss');
+
+// Gulp task to minify JavaScript files
+gulp.task('scripts', function () {
+  gulp
+    .src(['./*.js', '!./gulpfile.js'])
+    .pipe(
+      terser({
+        keep_fnames: true,
+        mangle: false,
+      })
+    )
+    .pipe(gulp.dest('./dist'));
+});
+
+// Gulp task to minify HTML files
+gulp.task('html', function () {
+  gulp
+    .src(['./*.html'])
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+        removeComments: true,
+        minifyCSS: true,
+      })
+    )
+    .pipe(gulp.dest('./dist'));
+});
+
+// Process tailwind
+gulp.task('css', function () {
+  const postcss = require('gulp-postcss');
+
+  return gulp
+    .src('style.css')
+    .pipe(
+      postcss([
+        require('tailwindcss'),
+        require('autoprefixer'),
+        require('gulp-purgecss'),
+      ])
+    )
+    .pipe(
+      purgecss({
+        content: ['**/*.html'],
+        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+      })
+    )
+    .pipe(gulp.dest('dist/'));
+});
+
+// Minify images
+gulp.task('minifyImages', function () {
+  gulp.src('./images/*').pipe(imagemin()).pipe(gulp.dest('./dist/images'));
+});
+
+// Move 'manifest.json'
+gulp.task('moveManifest', function () {
+  gulp.src('./manifest.json').pipe(gulp.dest('./dist'));
+});
+
+// Clean output directory
+gulp.task('clean', () => del(['dist']));
+
+//Watch files for changes
+gulp.task('watch-changes', (done) => {
+  //Watching HTML Files edits
+  gulp.watch('./**/*.html', ['html', 'css']);
+  //Watching JS Files edits
+  gulp.watch('./**/*.js', ['scripts']);
+});
+
+// Gulp task to minify all files
+gulp.task('default', ['clean'], function () {
+  runSequence(
+    'scripts',
+    'html',
+    'css',
+    'minifyImages',
+    'moveManifest',
+    'watch-changes'
+  );
+});
---
+++ index.js
@@ -0,0 +1,30 @@
+const wordInput = document.getElementById('wordInput');
+const definitionInput = document.getElementById('definitionInput');
+const submitBtn = document.getElementById('submitBtn');
+
+submitBtn.addEventListener('click', handleSubmit);
+
+function handleSubmit() {
+  const name = wordInput.value;
+  const definition = definitionInput.value;
+  postData('https://better-brain.herokuapp.com/wrds', {
+    name,
+    definition,
+  }).then((data) => {
+    console.log(data); // JSON data parsed by `response.json()` call
+    wordInput.value = '';
+    definition.value = '';
+  });
+}
+
+// Example POST method implementation:
+async function postData(url = '', data = {}) {
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
